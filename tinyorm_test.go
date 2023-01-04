7415ad6f7c2d51c6a4126eb41458a58d41092a91
Joshua Groeschl <joshua.groeschl@tutanota.com>
2023-01-03 18:26:26 -0800

    Removing dead code

--- tinyorm_test.go
+++ tinyorm_test.go
@@ -31,28 +31,6 @@ type Vehicle struct {
 
 type Vehicles []Vehicle
 
-func TestInitializeDatabase(t *testing.T) {
-	tests := []struct {
-		name string
-		have string
-		want string
-	}{
-		{
-			name: "",
-			have: "",
-			want: "",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			if test.have != test.want {
-				t.Errorf("test %s failed. Have: %v Want: %v", test.name, test.have, test.want)
-			}
-		})
-	}
-}
-
 func TestCreateUser(t *testing.T) {
 	db, err := tinyorm.Connect("development")
 

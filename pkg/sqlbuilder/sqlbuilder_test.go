b1f7ffd4bfcf63272aeffededddaba8bb14f6aea
Joshua Groeschl <joshua.groeschl@tutanota.com>
2023-01-12 18:28:46 -0800

    Fixing bugs within the snake casing code

--- pkg/sqlbuilder/sqlbuilder_test.go
+++ pkg/sqlbuilder/sqlbuilder_test.go
@@ -1,7 +1,26 @@
-package sqlbuilder_test
+package sqlbuilder
 
 import "testing"
 
 func TestSnakeCase(t *testing.T) {
+	tests := map[string]struct {
+		have string
+		want string
+	}{
+		"Test with ID enhanced": {have: "ID", want: "id"},
+		"Test with ID":          {have: "Id", want: "id"},
+		"Simple Test":           {have: "Testing", want: "testing"},
+		"Secondary Test":        {have: "HowNowBrownCow", want: "how_now_brown_cow"},
+		"Advanced Test":         {have: "TableWithNoID", want: "table_with_no_id"},
+		"Diff casing Test":      {have: "imma_Table", want: "imma__table"},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			lowerSnakeCaseString := lowerSnakeCase(test.have)
+			if test.want != lowerSnakeCaseString {
+				t.Fatalf("Wanted: %s - Have: %s", test.want, lowerSnakeCaseString)
+			}
+		})
+	}
 }
